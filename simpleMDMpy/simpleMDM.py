b027a742687a75e736a6bec048cf80c7408392be
Steve Küng <steve.kueng@srgssr.ch>
2017-11-01 12:59:05 +0100

    fix responsecode issues

--- simpleMDMpy/simpleMDM.py
+++ simpleMDMpy/simpleMDM.py
@@ -14,27 +14,31 @@ class connection:
 
     def _getData(self, url, data=None):
         resp = requests.get(url, data, auth=(self.apiKey, ""))
-        if resp.status_code != 200:
-            raise ApiError('GET {}'.format(resp.status_code), url)
-        return resp.json()
+        if resp.status_code 200 <= number <= 207:
+            return resp.json()
+        raise ApiError('GET {}'.format(resp.status_code), url)
+        
     
     def _patchData(self, url, data, files=None):
         resp = requests.patch(url, data, auth=(self.apiKey, ""), files=files)
-        if resp.status_code != 200 and resp.status_code != 204:
-            raise ApiError('PATCH {}'.format(resp.status_code), url)
-        return resp
+        if resp.status_code 200 <= number <= 207:
+            return resp
+        raise ApiError('PATCH {}'.format(resp.status_code), url)
     
     def _postData(self, url, data, files=None):
         resp = requests.post(url, data, auth=(self.apiKey, ""), files=files)
-        if resp.status_code != 200 and resp.status_code != 201:
-            raise ApiError('POST {}'.format(resp.status_code), url)
-        return resp
+        if resp.status_code 200 <= number <= 207:
+            return resp
+        raise ApiError('POST {}'.format(resp.status_code), url)
+        
     
     def _putData(self, url, data, files=None):
         resp = requests.put(url, data, auth=(self.apiKey, ""), files=files)
-        if resp.status_code != 200 and resp.status_code != 201:
-            raise ApiError('PUT {}'.format(resp.status_code), url)
-        return resp
+        if resp.status_code 200 <= number <= 207:
+            return resp
+        raise ApiError('PUT {}'.format(resp.status_code), url)
     
     def _deleteData(self, url):
-        return requests.delete(url, auth=(self.apiKey, ""))
+        if resp.status_code 200 <= number <= 207:
+            return requests.delete(url, auth=(self.apiKey, ""))
+        raise ApiError('DELETE {}'.format(resp.status_code), url)
